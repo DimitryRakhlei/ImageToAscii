3b1b39cea0068bf9d10c78df858861e3569ab0c3
dimitry rakhlei <dimitryrakhlei@hotmail.com>
2017-06-21 03:21:58 -0700

    cleaning up the code a bit

--- Fonts.py
+++ Fonts.py
@@ -13,11 +13,11 @@ class FontLoader :
         if not os.path.isdir('./letters/' + filename[:-4] + '/'):
             os.mkdir('./letters/' + filename[:-4] + '/')
 
-        for y in range(0, image.height,16):
-            for x in range(0, image.width,16):
-                sq = image.crop((x,y, x+16, y+16))
+        for y in range(0, image.height,this.framesize):
+            for x in range(0, image.width,this.framesize):
+                sq = image.crop((x,y, x+this.framesize, y+this.framesize))
                 letters.append(sq)
-                sq.save('./letters/{}/letter_{}_{}.png'.format(filename[:-4], int(x/16), int(y/16)))
+                sq.save('./letters/{}/letter_{}_{}.png'.format(filename[:-4], int(x/this.framesize), int(y/this.framesize)))
 
     def __LoadLib__(this, filename):
         this.fonts = []
@@ -28,16 +28,42 @@ class FontLoader :
                 if os.path.isfile(path + file):
                     this.fonts.append(Image.open(fp='./letters/' + filename[:-4] + '/' + file).convert('1'))
 
+    def ResizeFonts(this, size):
+        if size == this.fonts[0].height:
+            return
+        resized = []
+        for font in this.fonts:
+            resized.append(font.resize((size, size), Image.ANTIALIAS))
+        this.fonts = resized
+
+    def AdjustSize(this, image):
+        nwidth = -1
+        nheight = -1
+        print(image.size)
+        for i in range(image.width, 0, -1):
+            if i % this.framesize == 0:
+                nwidth = i
+                break
+        for i in range(image.height, 0, -1):
+            if i % this.framesize == 0:
+                nheight = i
+                break
+        if nwidth == -1 or nheight == -1:
+            print('fail')
+            return "fail"
+        image = image.resize((nwidth,nheight), Image.ANTIALIAS)
+        return image
+
     def GetAvgBrightness(this, img):
         avg = 0
         pixels = img.load()
-        for y in range(16):
-            for x in range(16):
+        for y in range(this.framesize):
+            for x in range(this.framesize):
                 avg += pixels[x,y]
         return avg / 256
 
     def GetLowestBrightness(this):
-        lowest = 999999999
+        lowest = sys.maxsize
         lowestIndex = -1
         for i in range(len(this.fonts)):
             avg = this.GetAvgBrightness(this.fonts[i])
@@ -63,38 +89,26 @@ class FontLoader :
 
     def MatchImageToAscii(this, image):
         print('matching image to ascii')
-        nwidth = -1
-        nheight = -1
-        print(image.size)
-        for i in range(image.width, 0, -1):
-            if i % 16 == 0:
-                nwidth = i
-                break
-        for i in range(image.height, 0, -1):
-            if i % 16 == 0:
-                nheight = i
-                break
-        if nwidth == -1 or nheight == -1:
-            print('fail')
-            return "fail"
-        image = image.resize((nwidth,nheight))
+        image = this.AdjustSize(image)
         data = image.load()
         print(image.size)
-        for y in range(0, image.height, 16):
-            for x in range(0, image.width, 16):
-                chunk = image.crop((x,y,x+16,y+16))
+        for y in range(0, image.height, this.framesize):
+            for x in range(0, image.width, this.framesize):
+                chunk = image.crop((x,y,x+this.framesize,y+this.framesize))
                 text = this.ChunkToTextValue(chunk).load()
-                for i in range(16):
-                    for j in range(16):
+                for i in range(this.framesize):
+                    for j in range(this.framesize):
                         data[x+j, y+i] = text[j,i]
         return image
 
-    def __init__(this, filename) :
+    def __init__(this, filename, framesize=16) :
+        this.framesize = framesize
         if os.path.isdir('./letters/' + filename[:-4] + '/'):
             this.__LoadLib__(filename)
             print('Letters already stored.')
             print('Loaded {} letters.'.format(len(this.fonts)))
             print('The size of the loaded array is: {} bytes.'.format(sys.getsizeof(this.fonts)))
+            this.ResizeFonts(framesize)
             this.ObtainAllBrightness()
             #this.ChunkToTextValue(this.fonts[1])
         else :
@@ -104,4 +118,7 @@ class FontLoader :
             print('Loaded {} letters.'.format(len(this.fonts)))
 
 
-floader = FontLoader(filename='bw_fonts.png')
+#floader = FontLoader(filename='bw_fonts.png', framesize=8)
+#test = Image.open('o1.png', mode="r").convert('1')
+#output = floader.MatchImageToAscii(test)
+#output.save('out.png')
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from Fonts0 import FontLoader
+from Fonts import FontLoader
 from PIL import Image
 import os
 
 def LoadImages(directory, font):
     if not os.path.isdir(os.curdir + '/' + directory + '/') :
+        print('directory does not exist')
         return
     path = './' + directory + '/'
     namegen = (file for __, __, file in os.walk('./inputs/'))
@@ -14,12 +15,12 @@ def LoadImages(directory, font):
             output = font.MatchImageToAscii(image)
             output.save('./outputs/' + file[:-4] + '_out'+ file[-4:])
 
-print('Gib folder')
-i = input()
-print('Gib tile size')
-t = input()
-floader = FontLoader(filename='bw_fonts.png', framesize=int(t))
-LoadImages(i, floader)
+print('Input Folder')
+imageFolderName = input()
+print('Input Tile Size')
+tileSize = input()
+font = FontLoader(filename='bw_fonts.png', framesize=int(tileSize))
+LoadImages(imageFolderName, font)
 #test = Image.open(i, mode="r").convert('1')
 #output = floader.MatchImageToAscii(test)
 #output.save(o)
